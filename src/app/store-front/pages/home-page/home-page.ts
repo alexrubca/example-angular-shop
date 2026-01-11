4db42fc4ca047ded233ae297751b5e18e5e80a7d
Alejandro Rubio <alejandrorubiocalvente@gmail.com>
2026-01-11 14:01:48 +0100

    feat: enhance product display with new components and services, add environment configuration

--- src/app/store-front/pages/home-page/home-page.ts
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ProductCard } from '@products/components/product-card/product-card';
+import { ProductsService } from '@products/services/products.service';
+
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [ ProductCard ],
   templateUrl: './home-page.html',
 })
-export class HomePage { }
+export class HomePage {
+  productsService = inject(ProductsService);
+
+  productsResource = rxResource({
+    params: () => ({}),
+    stream: () => {return this.productsService.getProducts({}) || []}
+  });
+}
--- src/app/store-front/pages/product-page/product-page.ts
+++ src/app/store-front/pages/product-page/product-page.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '@products/services/products.service';
+import { ProductCarousel } from '@products/components/product-carousel/product-carousel';
 
 @Component({
   selector: 'app-product-page',
-  imports: [],
+  imports: [ ProductCarousel ],
   templateUrl: './product-page.html',
 })
-export class ProductPage { }
+export class ProductPage {
+  activatedRoute = inject(ActivatedRoute);
+  productsService = inject(ProductsService);
+
+  idSlug = this.activatedRoute.snapshot.params['idSlug'];
+
+  productResource = rxResource({
+    params: () => ({ idSlug: this.idSlug }),
+    stream: ({ params }) => {
+      return this.productsService.getProductByIdSlug(params.idSlug);
+    }
+  });
+}
